a78e5b6aff0601fa71ba3dad7320d9d68404fe32
Indiana Lora <indiana@ensaludybelleza.com>
2022-05-06 18:04:43 -0400

    fetch song lyrics from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 
 import './App.css';
-
+import { useState, useEffect } from "react";
 function App() {
+  const artist = "Rosalia";
+  const song = "Con altura";
+  const apiUrl = `https://api.lyrics.ovh/v1/${artist}/${song}`
+
+  const lyricsFetcher = async () => {
+    const response = await fetch(`${apiUrl}`);
+    const data = await response.json();
+    console.log(data)
+  }
+  const [lyrics, setLyrics] = useState([])
+  useEffect(() => {
+    lyricsFetcher();
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-       <h1 class="center header">Training Manager</h1>
+        <h1 className="center header">Welcome to January</h1>
       </header>
-      <button className='addRutine'>Create a new rutine +</button>
+      <h3 className='description center'>Song searcher</h3>
     </div>
   );
 }
