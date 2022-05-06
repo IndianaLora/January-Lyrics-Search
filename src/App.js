1ae3a12097707d771611db5872d67ba89593f99d
Indiana Lora <indiana@ensaludybelleza.com>
2022-05-06 14:49:47 -0400

    web app to search songs lyrics

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1 class="center header">Training Manager</h1>
+      </header>
+      <button className='addRutine'>Create a new rutine +</button>
+    </div>
+  );
+}
+
+export default App;
